43eda5e8cc2e04b8705b00d4735696d47a4e61ce
githubscientist <sathish.datascientist@gmail.com>
2026-03-19 20:53:22 +0530

    1. express server setup --done\n2. dotenv setup --done

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
