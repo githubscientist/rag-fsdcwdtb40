0014b9d8ff31e420eb612b02ce4548187e761aeb
githubscientist <sathish.datascientist@gmail.com>
2026-03-19 20:56:22 +0530

    server and app code moved to their own files

--- server.js
+++ server.js
@@ -1,11 +1,5 @@
-const express = require('express');
+const app = require('./app');
 require('dotenv').config();
-
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
 
 const PORT = process.env.PORT || 3001;
 
