dd1970f774630b30c27e444673dce2716a0376f8
githubscientist <sathish.datascientist@gmail.com>
2026-03-19 21:02:24 +0530

    mongodb setup --done

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const app = require('./app');
-const { PORT } = require('./utils/config');
+const { PORT, MONGODB_URI } = require('./utils/config');
+const mongoose = require('mongoose');
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+mongoose
+    .connect(MONGODB_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error.message);
+    });
